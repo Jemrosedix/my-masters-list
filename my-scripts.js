cc60171385b983decc5af8f402b7afa4740d1f4c
Jemma <77214034+Jemrosedix@users.noreply.github.com>
2021-02-01 17:12:24 +1300

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,26 @@
+//store data 
+var myList = (JSON.parse(localStorage.getItem("myData")) || [])
+
+// create item
+function createItem(){
+	var newItem = {text:window.prompt("Add new MA task goals"),style:"none"}
+	myList.push (newItem)
+	localStorage.setItem("myData", JSON.stringify(myList))
+	printItem(newItem)
+	} 
+
+//print item
+function printItem(item){
+	//create the html 
+	//add the html to the 
+	var p = document. createElement("p")
+	p.innerHTML = item.text 
+	p.className = item.style
+	p.onclick = function (){
+		this.className = item.style = (this.className == "none") ? "strike" : "invisible"
+		localStorage.setItem("myData", JSON.stringify(myList))
+	}
+	document.getElementById("myDiv").appendChild (p)
+}
+//loop and print list
+for (x=0; x < myList.length; x++ ) {printItem(myList[x])}
